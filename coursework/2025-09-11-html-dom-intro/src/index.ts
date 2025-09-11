61b9d520710eaa17cd5be0c55db696e81b9d7ca2
MagicSev <severin.gusenleitner@gmail.com>
2025-09-11 16:41:33 +0200

    added select

--- coursework/2025-09-11-html-dom-intro/src/index.ts
+++ coursework/2025-09-11-html-dom-intro/src/index.ts
@@ -1,9 +1,14 @@
 const increaseButton = document.getElementById('countUp') as HTMLButtonElement;
 const counter = document.getElementById('counter') as HTMLParagraphElement;
 const decreaseButton = document.getElementById('countDown') as HTMLButtonElement;
+const increment = document.getElementById("increment") as HTMLInputElement;
+const select = document.getElementById("incrementValue") as HTMLSelectElement;
 let clickCount = 0;
+select.addEventListener('change', () => {
+    increment.value = select.value;
+});
 increaseButton.addEventListener('click', () => {
-  clickCount++;
+  clickCount += parseInt(increment.value);
   counter.textContent = `${clickCount}`;
   if (clickCount >= 0) {
     counter.style.color = 'black';
@@ -12,7 +17,7 @@ increaseButton.addEventListener('click', () => {
 
 
 decreaseButton.addEventListener('click', () => {
-  clickCount--;
+  clickCount -= parseInt(increment.value);
   counter.textContent = `${clickCount}`;
   if (clickCount < 0) {
     counter.style.color = 'red';

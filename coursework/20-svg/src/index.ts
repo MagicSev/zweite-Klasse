b4314a03bc05af3a4005a3334adc1f39ccbd962c
MagicSev <severin.gusenleitner@gmail.com>
2025-09-25 20:15:22 +0200

    ganzes beispiel ohne css

--- coursework/20-svg/src/index.ts
+++ coursework/20-svg/src/index.ts
@@ -2,6 +2,7 @@ const svg = document.getElementById("svg") as unknown as SVGSVGElement;
 const add = document.getElementById("add") as HTMLButtonElement;
 
 add.addEventListener("click", () =>{
+    
     const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle') as SVGCircleElement;
     circle.setAttribute("fill","lime")
     circle.setAttribute("stroke", "green")
@@ -9,5 +10,4 @@ add.addEventListener("click", () =>{
     circle.setAttribute("cx", `${Math.random()*200}`);
     circle.setAttribute("cy", `${Math.random()*200}`);
     svg.appendChild(circle);
-    
 })
